44199fe6a5c1f0697202c334cc2954c2310d21fd
Kevin2211875 <145025290+Kevin2211875@users.noreply.github.com>
2025-09-05 10:17:17 -0500

    Add files via upload

---
+++ SATSA/src/test/java/UIS/SATSA/SatsaApplicationTests.java
@@ -0,0 +1,13 @@
+package UIS.SATSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
